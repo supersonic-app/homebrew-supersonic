44173179bb193e459caa5ecc1524a5b1d5ca3f48
Alex Norton <alex@alexnorton.com>
2025-03-15 19:03:11 +0000

    Add formula

---
+++ Casks/supersonic.rb
@@ -0,0 +1,33 @@
+cask "supersonic" do
+  arch arm: "arm64", intel: "x64"
+
+  version "0.13.0"
+
+  sha256 :no_check
+
+  on_high_sierra do
+    url "https://github.com/dweymouth/supersonic/releases/download/v#{version}/Supersonic-#{version}-mac-legacy-HighSierra-x64.zip"
+  end
+
+  on_mojave :or_newer do
+    url "https://github.com/dweymouth/supersonic/releases/download/v#{version}/Supersonic-#{version}-mac-#{arch}.zip"
+  end
+
+  name "Supersonic"
+  desc "A lightweight and full-featured cross-platform desktop client for self-hosted music servers"
+  homepage "https://github.com/dweymouth/supersonic"
+
+  livecheck do
+    url :homepage
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Supersonic.app"
+
+  zap trash: [
+    "~/Library/Application Support/supersonic",
+    "~/Library/Caches/supersonic",
+  ]
+end
